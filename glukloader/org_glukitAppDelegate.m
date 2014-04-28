f6913316607996b8c081c08a334e4513c911fe1d
Alexandre Normand <alexandre.normand@gmail.com>
2014-04-27 21:24:50 -0700

    Update SyncEventObserver Signatures.

--- glukloader/org_glukitAppDelegate.m
+++ glukloader/org_glukitAppDelegate.m
@@ -213,18 +213,18 @@ static NSString *const CLIENT_ID = @"***REMOVED***";
 }
 
 - (void)syncStarted:(SyncEvent *)event {
-    NSLog(@"Sync started");
+    NSLog(@"Sync started at %@", [NSDate date]);
 }
 
 - (void)errorReadingReceiver:(SyncEvent *)event {
 
 }
 
-- (void)syncProgress:(SyncEvent *)event {
+- (void)syncProgress:(SyncProgressEvent *)event {
 
 }
 
-- (void)syncComplete:(SyncEvent *)event {
+- (void)syncComplete:(SyncCompletionEvent *)event {
     NSLog(@"Sync complete at %@", [NSDate date]);
 }
 
