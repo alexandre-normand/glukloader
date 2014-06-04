130b2f6bfc995b860757a7fcf57e889c1daf24bd
Alexandre Normand <alexandre.normand@gmail.com>
2014-06-03 21:38:10 -0700

    Add Mantle with CocoaPods instead of Manually Managed.

--- Podfile
+++ Podfile
@@ -1,2 +1,3 @@
 pod 'NXOAuth2Client', '~> 1.2.5'
 pod 'IYLoginItem'
+pod 'Mantle', '~> 1.5'
--- glukloaderTests/ModelConverterTests.m
+++ glukloaderTests/ModelConverterTests.m
@@ -8,7 +8,6 @@
 
 #import <XCTest/XCTest.h>
 #import <bloodSheltie/GlucoseRead.h>
-#import <bloodSheltie/EncodingUtils.h>
 #import "ModelConverter.h"
 #import "JsonEncoder.h"
 
@@ -18,39 +17,47 @@
 
 @implementation ModelConverterTests
 
-- (void)setUp
-{
+- (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
-- (void)tearDown
-{
+- (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
-- (void)testGlucoseReadsConversion
-{
+- (void)testGlucoseReadsConversion {
     NSMutableArray *glucoseRecords = [NSMutableArray array];
-    [glucoseRecords addObject:[GlucoseRead valueWithInternalTime:[NSDate dateWithTimeIntervalSinceNow:0] userTime:[NSDate dateWithTimeIntervalSinceNow:200] timezone:[NSTimeZone timeZoneWithName:@"America/Montreal"] value:4.2 unit:MMOL_PER_L]];
+    [glucoseRecords addObject:[GlucoseRead valueWithInternalTime:[NSDate dateWithTimeIntervalSinceNow:0]
+                                                        userTime:[NSDate dateWithTimeIntervalSinceNow:200]
+                                                        timezone:[NSTimeZone timeZoneWithName:@"America/Montreal"]
+                                                           value:4.2
+                                                            unit:MMOL_PER_L
+                                                       timestamp:0]];
 
     NSArray *glukitGlucoseReads = [ModelConverter convertGlucoseReads:glucoseRecords];
     NSArray *dictionaries = [MTLJSONAdapter JSONArrayFromModels:glukitGlucoseReads];
     NSError *error;
     NSString *json = [JsonEncoder encodeDictionaryArrayToJSON:dictionaries error:&error];
 
-    if (error == nil) {        
+    if (error == nil) {
         NSLog(@"Reads are %@", json);
     } else {
         XCTFail(@"Error encoding: %@", error);
     }
 }
 
-- (void)testInjectionConversion
-{
+- (void)testInjectionConversion {
     NSMutableArray *injections = [NSMutableArray array];
-    [injections addObject:[InsulinInjection valueWithInternalTime:[NSDate dateWithTimeIntervalSinceNow:0] userTime:[NSDate dateWithTimeIntervalSinceNow:200] userTimezone:[NSTimeZone timeZoneWithName:@"America/Montreal"] eventTime:[NSDate dateWithTimeIntervalSinceNow:130] insulinType:Basal unitValue:1.75 insulinName:@"Humalog"]];
+    [injections addObject:[InsulinInjection valueWithInternalTime:[NSDate dateWithTimeIntervalSinceNow:0]
+                                                         userTime:[NSDate dateWithTimeIntervalSinceNow:200]
+                                                     userTimezone:[NSTimeZone timeZoneWithName:@"America/Montreal"]
+                                                        eventTime:[NSDate dateWithTimeIntervalSinceNow:130]
+                                                      insulinType:Basal
+                                                        unitValue:1.75
+                                                      insulinName:@"Humalog"
+                                                        timestamp:0]];
 
     NSArray *glukitInjections = [ModelConverter convertInjections:injections];
     NSArray *dictionaries = [MTLJSONAdapter JSONArrayFromModels:glukitInjections];
@@ -64,10 +71,15 @@
     }
 }
 
-- (void)testCalibrationConversion
-{
+- (void)testCalibrationConversion {
     NSMutableArray *calibrations = [NSMutableArray array];
-    [calibrations addObject:[MeterRead valueWithInternalTime:[NSDate dateWithTimeIntervalSinceNow:0] userTime:[NSDate dateWithTimeIntervalSinceNow:110] timezone:[NSTimeZone timeZoneWithName:@"America/Montreal"] meterTime:[NSDate dateWithTimeIntervalSinceNow:230] meterRead:75.f glucoseMeasurementUnit:MG_PER_DL]];
+    [calibrations addObject:[MeterRead valueWithInternalTime:[NSDate dateWithTimeIntervalSinceNow:0]
+                                                    userTime:[NSDate dateWithTimeIntervalSinceNow:110]
+                                                    timezone:[NSTimeZone timeZoneWithName:@"America/Montreal"]
+                                                   meterTime:[NSDate dateWithTimeIntervalSinceNow:230]
+                                                   meterRead:75.f
+                                      glucoseMeasurementUnit:MG_PER_DL
+                                                   timestamp:0]];
 
     NSArray *glukitCalibrations = [ModelConverter convertCalibrationReads:calibrations];
     NSArray *dictionaries = [MTLJSONAdapter JSONArrayFromModels:glukitCalibrations];
@@ -81,10 +93,16 @@
     }
 }
 
-- (void)testMealConversion
-{
+- (void)testMealConversion {
     NSMutableArray *meals = [NSMutableArray array];
-    [meals addObject:[FoodEvent valueWithInternalTime:[NSDate dateWithTimeIntervalSinceNow:0] userTime:[NSDate dateWithTimeIntervalSinceNow:10] userTimezone:[NSTimeZone timeZoneWithName:@"America/Montreal"] eventTime:[NSDate dateWithTimeIntervalSinceNow:210] carbohydrates:10.f proteins:12.f fat:23.f]];
+    [meals addObject:[FoodEvent valueWithInternalTime:[NSDate dateWithTimeIntervalSinceNow:0]
+                                             userTime:[NSDate dateWithTimeIntervalSinceNow:10]
+                                         userTimezone:[NSTimeZone timeZoneWithName:@"America/Montreal"]
+                                            eventTime:[NSDate dateWithTimeIntervalSinceNow:210]
+                                        carbohydrates:10.f
+                                             proteins:12.f
+                                                  fat:23.f
+                                            timestamp:0]];
 
     NSArray *glukitMeals = [ModelConverter convertMeals:meals];
     NSArray *dictionaries = [MTLJSONAdapter JSONArrayFromModels:glukitMeals];
@@ -98,10 +116,16 @@
     }
 }
 
-- (void)testExerciseConversion
-{
+- (void)testExerciseConversion {
     NSMutableArray *exercises = [NSMutableArray array];
-    [exercises addObject:[ExerciseEvent valueWithInternalTime:[NSDate dateWithTimeIntervalSinceNow:0] userTime:[NSDate dateWithTimeIntervalSinceNow:10] userTimezone:[NSTimeZone timeZoneWithName:@"America/Montreal"] eventTime:[NSDate dateWithTimeIntervalSinceNow:20] duration:30 * 60.f intensity:LightExercise details:@"Walking"]];
+    [exercises addObject:[ExerciseEvent valueWithInternalTime:[NSDate dateWithTimeIntervalSinceNow:0]
+                                                     userTime:[NSDate dateWithTimeIntervalSinceNow:10]
+                                                 userTimezone:[NSTimeZone timeZoneWithName:@"America/Montreal"]
+                                                    eventTime:[NSDate dateWithTimeIntervalSinceNow:20]
+                                                     duration:30 * 60.f
+                                                    intensity:LightExercise
+                                                      details:@"Walking"
+                                                    timestamp:0]];
 
     NSArray *glukitExercises = [ModelConverter convertExercises:exercises];
     NSArray *dictionaries = [MTLJSONAdapter JSONArrayFromModels:glukitExercises];
