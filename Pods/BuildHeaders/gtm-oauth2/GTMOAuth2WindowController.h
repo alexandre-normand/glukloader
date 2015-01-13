75c4ee325b4bd2b839f775bbba5624b12cb09c8a
Alexandre Normand <alexandre.normand@gmail.com>
2015-01-12 21:10:46 -0800

    Release 1.0.2.

--- Pods/BuildHeaders/gtm-oauth2/GTMOAuth2WindowController.h
+++
@@ -1,1 +0,0 @@
-../../gtm-oauth2/Source/Mac/GTMOAuth2WindowController.h
---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,74 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// AFNetworking
+#define COCOAPODS_POD_AVAILABLE_AFNetworking
+#define COCOAPODS_VERSION_MAJOR_AFNetworking 1
+#define COCOAPODS_VERSION_MINOR_AFNetworking 1
+#define COCOAPODS_VERSION_PATCH_AFNetworking 0
+
+// GTMHTTPFetcher
+#define COCOAPODS_POD_AVAILABLE_GTMHTTPFetcher
+#define COCOAPODS_VERSION_MAJOR_GTMHTTPFetcher 1
+#define COCOAPODS_VERSION_MINOR_GTMHTTPFetcher 141
+#define COCOAPODS_VERSION_PATCH_GTMHTTPFetcher 0
+
+// IYLoginItem
+#define COCOAPODS_POD_AVAILABLE_IYLoginItem
+#define COCOAPODS_VERSION_MAJOR_IYLoginItem 0
+#define COCOAPODS_VERSION_MINOR_IYLoginItem 1
+#define COCOAPODS_VERSION_PATCH_IYLoginItem 0
+
+// Mantle
+#define COCOAPODS_POD_AVAILABLE_Mantle
+#define COCOAPODS_VERSION_MAJOR_Mantle 1
+#define COCOAPODS_VERSION_MINOR_Mantle 5
+#define COCOAPODS_VERSION_PATCH_Mantle 0
+
+// Mantle/extobjc
+#define COCOAPODS_POD_AVAILABLE_Mantle_extobjc
+#define COCOAPODS_VERSION_MAJOR_Mantle_extobjc 1
+#define COCOAPODS_VERSION_MINOR_Mantle_extobjc 5
+#define COCOAPODS_VERSION_PATCH_Mantle_extobjc 0
+
+// ReactiveCocoa
+#define COCOAPODS_POD_AVAILABLE_ReactiveCocoa
+#define COCOAPODS_VERSION_MAJOR_ReactiveCocoa 2
+#define COCOAPODS_VERSION_MINOR_ReactiveCocoa 3
+#define COCOAPODS_VERSION_PATCH_ReactiveCocoa 1
+
+// ReactiveCocoa/Core
+#define COCOAPODS_POD_AVAILABLE_ReactiveCocoa_Core
+#define COCOAPODS_VERSION_MAJOR_ReactiveCocoa_Core 2
+#define COCOAPODS_VERSION_MINOR_ReactiveCocoa_Core 3
+#define COCOAPODS_VERSION_PATCH_ReactiveCocoa_Core 1
+
+// ReactiveCocoa/UI
+#define COCOAPODS_POD_AVAILABLE_ReactiveCocoa_UI
+#define COCOAPODS_VERSION_MAJOR_ReactiveCocoa_UI 2
+#define COCOAPODS_VERSION_MINOR_ReactiveCocoa_UI 3
+#define COCOAPODS_VERSION_PATCH_ReactiveCocoa_UI 1
+
+// ReactiveCocoa/no-arc
+#define COCOAPODS_POD_AVAILABLE_ReactiveCocoa_no_arc
+#define COCOAPODS_VERSION_MAJOR_ReactiveCocoa_no_arc 2
+#define COCOAPODS_VERSION_MINOR_ReactiveCocoa_no_arc 3
+#define COCOAPODS_VERSION_PATCH_ReactiveCocoa_no_arc 1
+
+// gtm-oauth2
+#define COCOAPODS_POD_AVAILABLE_gtm_oauth2
+#define COCOAPODS_VERSION_MAJOR_gtm_oauth2 0
+#define COCOAPODS_VERSION_MINOR_gtm_oauth2 1
+#define COCOAPODS_VERSION_PATCH_gtm_oauth2 0
+
+// mailgun
+#define COCOAPODS_POD_AVAILABLE_mailgun
+#define COCOAPODS_VERSION_MAJOR_mailgun 1
+#define COCOAPODS_VERSION_MINOR_mailgun 0
+#define COCOAPODS_VERSION_PATCH_mailgun 3
+
